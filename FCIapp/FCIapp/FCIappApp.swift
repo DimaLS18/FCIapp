2b720250e3719bdc01df3296bd36aafc9599711b
DimaLS18 <kovrigindima@gmail.com>
2023-01-31 18:34:35 +0700

    main

---
+++ FCIapp/FCIapp/FCIappApp.swift
@@ -0,0 +1,17 @@
+//
+//  FCIappApp.swift
+//  FCIapp
+//
+//  Created by Dima Kovrigin on 31.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct FCIappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
